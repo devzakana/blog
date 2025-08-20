b1caf749a02ad5d07e3829a0ed4fdfe5faba22b2
devzakana <kozakana.t7o@gmail.com>
2025-08-20 23:40:30 +0900

    add: deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
@@ -8,6 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react()],
+  adapter: cloudflare(),
 });
