0555c0ca94930d81bee3d50665cca0fc790b3ed0
devzakana <kozakana.t7o@gmail.com>
2025-08-20 21:45:27 +0900

    setup shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 // @ts-check
+
+import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
@@ -6,4 +8,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
